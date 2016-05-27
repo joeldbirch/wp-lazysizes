e367bbf5b4ad2d313ec8ccc58dc0a2c6a89a8d10
Joel Birch <joeldbirch@gmail.com>
2016-05-27 21:53:36 +1000

    Use client-side mobile detection by prepending lazysizes.min.js with extra JS from ls.setup.js via 'grunt build'. Use resulting JS file in wp_enqueue_script using the same 'lazysizes' handle.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,13 +15,24 @@
 						}
 					]
 				}
+			},
+
+			concat: {
+				dist: {
+					src: ['js/ls.setup.js', 'js/lazysizes/lazysizes.min.js'],
+					dest: 'build/wp-lazysizes.min.js',
+				},
 			}
 		});
 
 		grunt.loadNpmTasks('grunt-contrib-copy');
+		grunt.loadNpmTasks('grunt-contrib-concat');
 
 
 		// Default task.
 		grunt.registerTask('default', [ 'copy' ]);
+
+		// Concatenate lazysizes JS with mobile detection JS files.
+		grunt.registerTask('build', [ 'concat' ]);
 	};
 })();
